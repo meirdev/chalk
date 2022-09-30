3f4b90a8baf33eb2178f9b1b870d979a4a54825a
meirdev <maornet@gmail.com>
2022-09-30 07:14:09 +0300

    format

--- example.py
+++ example.py
@@ -13,18 +13,22 @@ print(chalk.blue("Hello", "World!", "Foo", "bar", "biz", "baz"))
 print(chalk.red("Hello", chalk.underline.bg_blue("world") + "!"))
 
 # Nest styles of the same type even (color, underline, background)
-print(chalk.green(
-	"I am a green line " +
-	chalk.blue.underline.bold("with a blue substring") +
-	" that becomes green again!"
-))
+print(
+    chalk.green(
+        "I am a green line "
+        + chalk.blue.underline.bold("with a blue substring")
+        + " that becomes green again!"
+    )
+)
 
 # f-string
-print(f"""
+print(
+    f"""
 CPU: {chalk.red("90%")}
 RAM: {chalk.green("40%")}
 DISK: {chalk.yellow("70%")}
-""")
+"""
+)
 
 # Use RGB colors in terminal emulators that support it.
 print(chalk.rgb(123, 45, 67).underline("Underlined reddish color"))
@@ -32,7 +36,7 @@ print(chalk.hex("#DEADED").bold("Bold gray!"))
 
 # Easily define your own themes
 error = chalk.bold.red
-warning = chalk.hex('#FFA500')
+warning = chalk.hex("#FFA500")
 
-print(error('Error!'))
-print(warning('Warning!'))
+print(error("Error!"))
+print(warning("Warning!"))
